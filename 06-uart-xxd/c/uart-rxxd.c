a4613ea20079557facd7a1339d227900c8deec33
Ashley Davis <ashley_davis10419@hotmail.com>
2019-05-31 00:17:11 +0100

    add lots of working + docs for combining bytes to make a word we can write

--- 06-uart-xxd/c/uart-rxxd.c
+++ 06-uart-xxd/c/uart-rxxd.c
@@ -13,67 +13,65 @@
 #define UART0_IP        (UART0BASE+0x14)
 #define UART0_DIV       (UART0BASE+0x18)
 
-inline static uint32_t
-__attribute__((always_inline))
-GET32(uint32_t addr) {
-	return *((volatile uint32_t *) addr);
+inline static uint32_t __attribute__((always_inline)) GET32(uint32_t addr) {
+	return *((volatile uint32_t *volatile) addr);
 }
 
-inline static void
-__attribute__((always_inline))
-PUT32(uint32_t addr, uint32_t val) {
+inline static void __attribute__((always_inline)) PUT32(uint32_t addr, uint32_t val) {
 	*((volatile uint32_t *) addr) = val;
 }
 
-inline static void
-__attribute__((always_inline))
-uart_init (void) {
-    unsigned int ra = GET32(GPIO_IOF_SEL);
+inline static void __attribute__((always_inline)) uart_init (void) {
+	unsigned int ra = GET32(GPIO_IOF_SEL);
 
-    ra &= ~(1<<16);
-    ra &= ~(1<<17);
+	ra &= ~(1<<16);
+	ra &= ~(1<<17);
 
-    PUT32(GPIO_IOF_SEL,ra);
+	PUT32(GPIO_IOF_SEL,ra);
 
-    ra = GET32(GPIO_IOF_EN);
-    ra |= 1<<16;
-    ra |= 1<<17;
+	ra = GET32(GPIO_IOF_EN);
+	ra |= 1<<16;
+	ra |= 1<<17;
 
-    PUT32(GPIO_IOF_EN, ra);
+	PUT32(GPIO_IOF_EN, ra);
 
-    PUT32(UART0_DIV, 138);
-    PUT32(UART0_TXCTRL, 0x00000003);
-    PUT32(UART0_RXCTRL, 0x00000001);
+	PUT32(UART0_DIV, 138);
+	PUT32(UART0_TXCTRL, 0x00000003);
+	PUT32(UART0_RXCTRL, 0x00000001);
 }
 
 void __attribute__((noreturn)) xmain(void) {
-	uart_init();
+	//uart_init();
 
 	uint32_t x05 = 0x00;
 	uint32_t x06 = 0x00;
+	uint32_t x07 = 0x04;
 	uint32_t x08 = 0x00;
 	uint32_t x09 = 0x00;
 	uint32_t x10 = 0x00;
+	uint32_t x11 = 0x00;
 	uint32_t x12 = 0x80000000;
 	uint32_t x14 = 0x80010000;
 	uint32_t x15 = UART0_TXDATA;
 	uint32_t x16 = UART0_RXDATA;
 	uint32_t x17 = 0xA;
+	uint32_t x18 = 0x20;
 
 READ_UART:
-	x10 = GET32(x16);
-	x10 &= x12;
-	if (x10 != 0) { goto READ_UART; }
+	//x10 = GET32(x16);
+	x10 = *((volatile uint32_t *) x16);
+	x11 = x10 & x12;
+	if (x11 != 0) { goto READ_UART; }
 
 	x10 &= 0xFF;
 
-	if (x08 != 0) { goto NOT_COMMENT_MODE; }
+	if (x08 == 0) { goto NOT_COMMENT_MODE; }
 
 	if (x10 != x17) { goto READ_UART; }
 
 	x08 = 0;
 	goto READ_UART;
-	
+
 NOT_COMMENT_MODE:
 	x09 = 0x23;
 	if (x10 != x09) { goto NO_START_COMMENT_MODE; }
@@ -103,7 +101,24 @@ PARSE_CHAR:
 	goto ADDRAW;
 
 ADDRAW:
-	asm("nop");
+	x10 <<= x07;
+	x09 |= x10;
+	if (x07 == 0) { goto ADDBYTE; }
+	x07 = 0x0;
+	goto READ_UART;
+
+ADDBYTE:
+	x07 = 0x4;
+	x09 <<= x06;
+	x05 |= x09;
+	x06 += 0x8;
+	if (x06 == x18) { goto WRITEWORD; }
+	goto READ_UART;
+
+WRITEWORD:
+	PUT32(x14, x05);
+	x14 += 4;
+	goto READ_UART;
 
 PANIC:
 	for(;;){}
