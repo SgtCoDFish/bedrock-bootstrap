0166417fb28fc5e78c42d31a2ff325228d09fa49
Ashley Davis <ashley_davis10419@hotmail.com>
2019-07-14 21:34:22 +0100

    Start working on library to interface with GDB for RISC-V

--- substratum/cmd/substratum/autotest_cmd.go
+++ substratum/cmd/substratum/autotest_cmd.go
@@ -1,32 +1,116 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/sgtcodfish/substratum"
 
 	"github.com/cyrus-and/gdb"
+
+	"github.com/mitchellh/mapstructure"
 )
 
-func processAutotest(logger *log.Logger) error {
-	// TODO: properly load gdb path
-	conn, err := gdb.NewCmd([]string{"riscv32-unknown-elf-gdb", "--nx", "--quiet", "--interpreter=mi2", "-ex", "set architecture riscv:rv32"}, nil)
+type GdbConnection struct {
+	Conn   *gdb.Gdb
+	Logger *log.Logger
+
+	// TODO: fill these in!
+	registerList    []string
+	registerNumbers []string
+}
+
+type gdbRegisterNamesResponse struct {
+	Payload gdbRegisterNamesResponsePayload `json:"payload",mapstructure:"payload"`
+}
+
+type gdbRegisterNamesResponsePayload struct {
+	RegisterNames []string `json:"register-names",mapstructure:"register-names"`
+}
+
+func NewGDBConnection(logger *log.Logger, gdbPath string, remoteTarget string) (*GdbConnection, error) {
+	conn, err := gdb.NewCmd([]string{gdbPath, "--nx", "--quiet", "--interpreter=mi2", "-ex", "set architecture riscv:rv32"}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = conn.CheckedSend(fmt.Sprintf("target-select remote %s", remoteTarget))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := conn.CheckedSend("data-list-register-names")
+	if err != nil {
+		return nil, err
+	}
+
+	var registerNames gdbRegisterNamesResponse
+	err = mapstructure.Decode(resp, &registerNames)
+	if err != nil {
+		return nil, err
+	}
+
+	registerList := substratum.GetRegisterList()
+	names := resp["payload"].(map[string]interface{})["register-names"].([]interface{})
+	registerNumbers := make([]string, len(registerList))
+
+	return &GdbConnection{
+		Logger: logger,
+		Conn:   conn,
+	}, nil
+}
+
+func (s *GdbConnection) dumpIntegerRegisters() error {
+	resp, err := s.Conn.CheckedSend("data-list-register-names")
 	if err != nil {
 		return err
 	}
 
-	resp, err := conn.Send("target-select remote :1234")
+	registerList := substratum.GetRegisterList()
+	names := resp["payload"].(map[string]interface{})["register-names"].([]interface{})
+	registerNumbers := make([]string, len(registerList))
+
+	for i, regName := range registerList {
+		for j, foundNameRaw := range names {
+			foundName := foundNameRaw.(string)
+			if regName == foundName {
+				registerNumbers[i] = strconv.Itoa(j)
+				break
+			}
+		}
+	}
+
+	resp, err = s.Conn.CheckedSend(fmt.Sprintf("data-list-register-values x %s", strings.Join(registerNumbers, " ")))
+	if err != nil {
+		return err
+	}
+
+	s.Logger.Printf("%+v", resp)
+
+	return nil
+}
+
+func processAutotest(logger *log.Logger) error {
+	gdbPath := os.Getenv("RISCV_PREFIX") + "gdb"
+
+	gdbConn, err := NewGDBConnection(logger, gdbPath)
 	if err != nil {
 		return err
 	}
 
 	logger.Printf("%+v", resp)
 
-	for i := 0; i < 100; i++ {
-		resp, err = conn.Send("exec-step-instruction")
+	for i := 0; i < 1; i++ {
+		resp, err = gdbConn.Conn.CheckedSend("exec-step-instruction")
 		if err != nil {
 			return err
 		}
 
 		logger.Printf("%+v", resp)
 	}
+
 	return nil
 }
--- substratum/go.mod
+++ substratum/go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/cyrus-and/gdb v0.0.0-20180213171718-0306a029f42f
 	github.com/kr/pty v1.1.8 // indirect
+	github.com/mitchellh/mapstructure v1.1.2
 	golang.org/x/sys v0.0.0-20190710143415-6ec70d6a5542 // indirect
 )
