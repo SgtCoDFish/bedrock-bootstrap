ded4295d3532fd5182183104535703769ec6bf11
Ashley Davis <ashley_davis10419@hotmail.com>
2024-01-14 21:49:27 +0000

    working towards fixing asm test
    Signed-off-by: Ashley Davis <ashley_davis10419@hotmail.com>

--- substratum/cmd/ss-asm/main/main_test.go
+++ substratum/cmd/ss-asm/main/main_test.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -21,51 +22,72 @@ func (c *closerBuffer) Close() error {
 type testCase struct {
 	args           []string
 	input          string
+	outputFormat   string
 	expectedOutput []byte
 }
 
 func TestSsASM(t *testing.T) {
-	t.Parallel()
-
-	var cases = []testCase{
-		{
-			input:          "bne a0 x0 -8",
-			args:           []string{"-input", "-", "-output", "-", "-output-format", "hex"},
+	var cases = map[string]testCase{
+		"hex output": {
+			input:          "bne a0 x0 -8\n",
+			args:           []string{"-input", "-", "-output", "-"},
+			outputFormat:   "hex",
 			expectedOutput: []byte("fe 05 1c e3 # bne a0 x0 -8\n"),
 			// 0xFE051CE3
 		},
-		{
-			input:          "bne a0 x0 -8",
-			args:           []string{"-input", "-", "-output", "-", "-output-format", "hex"},
-			expectedOutput: []byte{byte(0xFE), byte(0x05), byte(0x1C), byte(0xE3)},
+		"bin output": {
+			input:          "bne a0 x0 -8\n",
+			args:           []string{"-input", "-", "-output", "-"},
+			outputFormat:   "bin",
+			expectedOutput: []byte{byte(0xE3), byte(0x1C), byte(0x05), byte(0xFE)},
 			// 0xFE051CE3
 		},
 	}
 
-	for _, c := range cases {
-		ctx := context.Background()
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			ctx := context.Background()
 
-		outBuf := &closerBuffer{}
+			c.args = append(c.args, "-output-format")
+			c.args = append(c.args, c.outputFormat)
 
-		invocation, err := ssasm.ParseInvocation("ss-asm-test", c.args)
-		if err != nil {
-			t.Errorf("failed to set up test with args '(%v)': %v", c.args, err)
-			continue
-		}
+			invocation, err := ssasm.ParseInvocation("ss-asm-test", c.args)
+			if err != nil {
+				t.Errorf("failed to set up test with args '(%v)': %v", c.args, err)
+				return
+			}
 
-		invocation.Input = bufio.NewReader(strings.NewReader(c.input))
-		invocation.Output = outBuf
+			outBuf := &closerBuffer{}
 
-		err = invocation.Run(ctx)
-		if err != nil {
-			t.Errorf("got an error response from running ss-asm(%v): %v", c.args, err)
-			continue
-		}
+			invocation.Input = bufio.NewReader(strings.NewReader(c.input))
+			invocation.Output = outBuf
 
-		output := outBuf.Bytes()
+			err = invocation.Run(ctx)
+			if err != nil {
+				t.Errorf("got an error response from running ss-asm(%v): %v", c.args, err)
+				return
+			}
 
-		if bytes.Equal(output, c.expectedOutput) {
-			t.Errorf("got unexpected output from input %q", c.input)
-		}
+			output := outBuf.Bytes()
+
+			if !bytes.Equal(output, c.expectedOutput) {
+				var renderedOutput string
+				var wantedOutput string
+
+				if c.outputFormat == "bin" {
+					renderedOutput = fmt.Sprintf("%+v", output)
+					wantedOutput = fmt.Sprintf("%+v", c.expectedOutput)
+				} else if c.outputFormat == "hex" {
+					renderedOutput = fmt.Sprintf("%s", string(output))
+					wantedOutput = fmt.Sprintf("%s", string(c.expectedOutput))
+				} else {
+					t.Errorf("test setup error: unknown output format %q", c.outputFormat)
+					renderedOutput = "<unknown>"
+				}
+
+				t.Errorf("got unexpected output from input %q\nwanted: %+v\ngot   : %s\n", c.input, wantedOutput, renderedOutput)
+				return
+			}
+		})
 	}
 }
